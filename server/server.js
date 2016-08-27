15ea63745edf7f44812156dc9a39babf1d964bc0
Francisco A. C. Souza <francisceioseph@gmail.com>
2016-08-27 20:16:56 -0300

    Adição de suporte ao angular material. Atualização do .gitignore Toolbar e botões no home.html

--- server/server.js
+++ server/server.js
@@ -10,10 +10,9 @@ app.use(express.static('www'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.get('*', function(req, res){
+app.get('/', function(req, res){
   res.sendFile('./www/index.html');
 });
-
 
 app.listen(8008);
 console.log('Server listening on localhost:8008');
--- www/app.js
+++ www/app.js
@@ -1,14 +1,18 @@
 'use strict';
 
-angular.module('brand-sample', ['ui.router'])
+angular.module('brand-sample', ['ui.router', 'ngMaterial'])
   .config(brandSampleConfig);
 
-brandSampleConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-function brandSampleConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+brandSampleConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider', '$mdThemingProvider'];
+function brandSampleConfig($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
  $urlRouterProvider.otherwise('/home');
 
  $stateProvider.state('home', {
    url: '/home',
    templateUrl: 'views/home/home.html'
  });
+
+ $mdThemingProvider.theme('default')
+  .primaryPalette('teal')
+  .accentPalette('orange');
 }
